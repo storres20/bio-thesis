43fb0e0cce510c3a6db090300f3e5d818755d985
storres20 <sanchotorres777@gmail.com>
2024-06-23 15:48:18 -0500

    Update home page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,5 @@
 'use client'
 
-/*export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello world</h1>
-    </main>
-  );
-}*/
-
 import { useAuth } from '@/context/AuthContext';
 
 export default function Home() {

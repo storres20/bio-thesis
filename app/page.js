6cd8c82b28344e538512a8d97ca6d39f21bd0631
storres20 <sanchotorres777@gmail.com>
2024-06-27 14:15:06 -0500

    Update home page

--- app/page.js
+++ app/page.js
@@ -1,20 +1,9 @@
-'use client'
-
-import { useAuth } from '@/context/AuthContext';
+'use client';
 
 export default function Home() {
-    const { logout } = useAuth();
-
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main>
             <h1>Hello world</h1>
-            <button
-                onClick={logout}
-                className="mt-4 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"
-            >
-                Logout
-            </button>
         </main>
     );
 }
-

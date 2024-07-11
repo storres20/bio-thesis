6e77488998c1ca0f3dbf07c155357e59e4ac7123
storres20 <sanchotorres777@gmail.com>
2024-07-10 19:37:23 -0500

    Add datatable

--- app/inventory/[id]/page.js
+++ app/inventory/[id]/page.js
@@ -1,11 +1,11 @@
 // app/inventories/[id]/page.js
 'use client'
-
 import { useEffect, useState } from 'react';
 
 const InventoryDetail = ({ params }) => {
     const { id } = params;
     const [item, setItem] = useState(null);
+    const [historyData, setHistoryData] = useState(null);
 
     useEffect(() => {
         if (id) {
@@ -13,10 +13,16 @@ const InventoryDetail = ({ params }) => {
                 .then(response => response.json())
                 .then(data => setItem(data))
                 .catch(error => console.error('Error fetching item:', error));
+
+            fetch(`http://localhost:3001/api/v1/historials/getByInventory/${id}`)
+                .then(response => response.json())
+                .then(data => setHistoryData(data))
+                .catch(error => console.error('Error fetching history:', error));
         }
     }, [id]);
 
-    if (!item) return <div>Loading...</div>;
+    // Check if the details and historyData have been set. If not, still loading.
+    if (!item || !historyData) return <div>Loading...</div>;
 
     return (
         <div>
@@ -26,8 +32,42 @@ const InventoryDetail = ({ params }) => {
             <p>Serie: {item.serie}</p>
             <p>Location: {item.location}</p>
             <p>Codepat: {item.codepat}</p>
+            <br/>
+            <h2>History</h2>
+            {historyData.length > 0 ? (
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Info</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        historyData.map((entry, i) => (
+                            <tr key={i}>
+                                <td>{entry.info}</td>
+                            </tr>
+                        ))
+                    }
+                    </tbody>
+                </table>
+            ) : (
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Info</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>NO DATA TO SHOW</td>
+                        </tr>
+                    </tbody>
+
+                </table>
+
+            )}
         </div>
     );
 };
-
 export default InventoryDetail;

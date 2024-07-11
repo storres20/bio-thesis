6836a4149f78fa1936b90db06c903f1570041009
storres20 <sanchotorres777@gmail.com>
2024-07-10 19:37:50 -0500

    Update home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,29 @@
-'use client';
+'use client'
+
+import React from 'react';
+import { setCookie, parseCookies, destroyCookie } from 'nookies';
 
 export default function Home() {
+
+    const cookies = parseCookies();
+    console.log('app/pages')
+    const users_id = cookies.users_id
+    const hospitals_id = cookies.hospitals_id
+    const hospitals_name = cookies.hospitals_name
+    const profile = cookies.profile
+
     return (
         <main>
-            <h1>Hello world</h1>
+            {/*<h1>User email: {user.email}</h1>
+            <p>User password: {user.password}</p>
+            <p>User hospital: {user.hospitals_id}</p>
+            <p>User id: {user._id}</p>*/}
+            <h1>hello world</h1>
+            <p>user id : {users_id}</p>
+            <p>hospital id : {hospitals_id}</p>
+            <p>hospital name : {hospitals_name}</p>
+            <p>profile : {profile}</p>
+
         </main>
     );
 }

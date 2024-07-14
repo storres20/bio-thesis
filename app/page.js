32ebf4579b8362449f7527b779600252965a1ec9
storres20 <sanchotorres777@gmail.com>
2024-07-14 16:40:09 -0500

    Update home page

--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ import { setCookie, parseCookies, destroyCookie } from 'nookies';
 export default function Home() {
 
     const cookies = parseCookies();
-    console.log('app/pages')
     const users_id = cookies.users_id
     const hospitals_id = cookies.hospitals_id
     const hospitals_name = cookies.hospitals_name
@@ -14,11 +13,8 @@ export default function Home() {
 
     return (
         <main>
-            {/*<h1>User email: {user.email}</h1>
-            <p>User password: {user.password}</p>
-            <p>User hospital: {user.hospitals_id}</p>
-            <p>User id: {user._id}</p>*/}
-            <h1>hello world</h1>
+
+            <h1>Welcome!!</h1>
             <p>user id : {users_id}</p>
             <p>hospital id : {hospitals_id}</p>
             <p>hospital name : {hospitals_name}</p>

1f8e207e03884ae35d83b04a4c4189ab46a88f07
storres20 <sanchotorres777@gmail.com>
2024-07-19 20:38:32 -0500

    Updata otm page to avoid conflicts with datatable during deploy

--- app/otm/page.js
+++ app/otm/page.js
@@ -1,12 +1,15 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 /* DataTable */
-import $ from 'jquery';
-import 'datatables.net-dt/css/dataTables.dataTables.css';
-import 'datatables.net';
+// Dynamic Import
+const DataTableComponent = dynamic(
+    () => import('../../components/DatatableOtm'),
+    { ssr: false }
+);
 /************************/
 
 import {parseCookies} from "nookies";
@@ -54,19 +57,9 @@ const OtmPage = () => {
             .catch(error => console.error('Error fetching items:', error));
     };
 
-    /* DataTable useEffect */
-    useEffect(() => {
-        if (items.length > 0 && !dataLoaded) {
-            // Initialize DataTable
-            $('#example').DataTable();
-            setDataLoaded(true);
-        }
-    }, [items, dataLoaded]);
-
     const newotm = () => {
       router.push('/newotm')
     }
-
 
 
     return (
@@ -76,51 +69,7 @@ const OtmPage = () => {
 
             {/* DataTable*/}
             <div className="overflow-x-auto">
-                <table id="example" className="display">
-                    <thead>
-                    <tr>
-                        <th>Fecha de solicitud</th>
-                        <th>Usuario inicial</th>
-                        <th>Estado</th>
-                        <th>Codigo patrimonial</th>
-                        <th>Nombre del equipo</th>
-                        <th>Servicio/Departamento</th>
-                        <th>Problema</th>
-                        <th>Solucion</th>
-                        <th>Fecha de cierre</th>
-                        <th>Usuario final</th>
-                        <th>Atencion</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {items.map(item => (
-                        <tr key={item._id}>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.fecha_open}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.usersid_open?.email}</td>
-                            <td
-                                className={`px-6 py-4 whitespace-nowrap ${item.estado === 'open' ? 'text-blue-500' : item.estado === 'close' ? 'text-red-500' : 'text-black'} `}
-                            >
-                                {item.estado}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.inventories_id.codepat}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.inventories_id.name}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.servicio}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.problema}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.solucion}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.fecha_close}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.usersid_close?.email}</td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                                {item.estado === "open" && (
-                                    <button
-                                        onClick={() => router.push(`/checkotm/${item._id}`)}
-                                        className="bg-blue-500 text-white p-2 rounded">Check
-                                    </button>
-                                )}
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
+                <DataTableComponent items={items} />
             </div>
         </div>
     );

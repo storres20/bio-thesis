26b0f442e51670f74be8c6440b2cf4c9698b3e48
storres20 <sanchotorres777@gmail.com>
2024-07-21 15:34:38 -0500

    Refactor inventory/id page

--- app/inventory/[id]/page.js
+++ app/inventory/[id]/page.js
@@ -1,81 +1,24 @@
 // app/inventories/[id]/page.js
 'use client'
-import { useEffect, useState } from 'react';
 
-import config from '@/config'; //for apiUrl
+import dynamic from 'next/dynamic';
+
+/* DataTable */
+// Dynamic Import
+const DataTableComponent = dynamic(
+    () => import('../../../components/DatatableInventoryId'),
+    { ssr: false }
+);
+/************************/
 
 const InventoryDetail = ({ params }) => {
 
     const { id } = params;
-    const [item, setItem] = useState(null);
-    const [historyData, setHistoryData] = useState(null);
-
-    useEffect(() => {
-        if (id) {
-            fetch(`${config.apiUrl}/inventories/${id}`)
-                .then(response => response.json())
-                .then(data => setItem(data))
-                .catch(error => console.error('Error fetching item:', error));
-
-            fetch(`${config.apiUrl}/historials/getByInventory/${id}`)
-                .then(response => response.json())
-                .then(data => setHistoryData(data))
-                .catch(error => console.error('Error fetching history:', error));
-        }
-    }, [id]);
-
-    // Check if the details and historyData have been set. If not, still loading.
-    if (!item || !historyData) return <div>Loading...</div>;
 
     return (
-        <div>
-            <h1>Name: {item.name}</h1>
-            <p>Brand: {item.brand}</p>
-            <p>Model: {item.model}</p>
-            <p>Serie: {item.serie}</p>
-            <p>Location: {item.location}</p>
-            <p>Codepat: {item.codepat}</p>
-            <br/>
-            <h2>History</h2>
-            {historyData.length > 0 ? (
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Fecha de Solicitud</th>
-                        <th>Problema</th>
-                        <th>Solucion</th>
-                        <th>Estado</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        historyData.map((entry, i) => (
-                            <tr key={i}>
-                                <td>{entry.fecha_open}</td>
-                                <td>{entry.problema}</td>
-                                <td>{entry.solucion}</td>
-                                <td>{entry.estado}</td>
-                            </tr>
-                        ))
-                    }
-                    </tbody>
-                </table>
-            ) : (
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Info</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>NO DATA TO SHOW</td>
-                        </tr>
-                    </tbody>
-
-                </table>
-
-            )}
+        <div className="p-8">
+            {/* DataTable*/}
+            <DataTableComponent id={id} />
         </div>
     );
 };

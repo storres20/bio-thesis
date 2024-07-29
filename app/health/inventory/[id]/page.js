65945ec21073e7aa816612f23c1cf850374309ab
storres20 <sanchotorres777@gmail.com>
2024-07-29 12:08:37 -0500

    Move files inside Health folder

---
+++ app/health/inventory/[id]/page.js
@@ -0,0 +1,35 @@
+// app/inventories/[id]/page.js
+'use client'
+
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+
+/* DataTable */
+// Dynamic Import
+const DataTableComponent = dynamic(
+    () => import('@/components/DatatableInventoryId'),
+    { ssr: false }
+);
+/************************/
+
+const InventoryDetail = ({ params }) => {
+
+    const { id } = params;
+
+    /* Router */
+    const router = useRouter();
+
+    const returnBack = () => {
+        router.push('/health/inventory');
+    }
+
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl mb-6">Inventory by ID</h1>
+            <button onClick={returnBack} className="bg-red-500 text-white p-2 rounded">Return back</button>
+            {/* DataTable*/}
+            <DataTableComponent id={id}/>
+        </div>
+    );
+};
+export default InventoryDetail;

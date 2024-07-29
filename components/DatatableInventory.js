2616cd29e00a06101b657009a279c6ccced8c8b3
storres20 <sanchotorres777@gmail.com>
2024-07-29 12:01:56 -0500

    Update page for adding modals

--- components/DatatableInventory.js
+++ components/DatatableInventory.js
@@ -7,6 +7,8 @@ import config from '@/config'; //for apiUrl
 import {useEffect, useState} from 'react';
 import { useRouter } from 'next/navigation';
 
+import {parseCookies} from "nookies";
+
 const DataTableComponent = ({ items = [], setItems }) => {
     /* useState - edit item */
     const [editModalOpen, setEditModalOpen] = useState(false);
@@ -18,6 +20,25 @@ const DataTableComponent = ({ items = [], setItems }) => {
     const [editLocation, setEditLocation] = useState('');
     const [editCodepat, setEditCodepat] = useState('');
 
+    /* useState - newotm item */
+    const [newotmModalOpen, setNewotmModalOpen] = useState(false)
+    const [newotmItemId, setNewotmItemId] = useState(null);
+    const [newotmName, setNewotmName] = useState('');
+    const [newotmBrand, setNewotmBrand] = useState('');
+    const [newotmModel, setNewotmModel] = useState('');
+    const [newotmSerie, setNewotmSerie] = useState('');
+    const [newotmLocation, setNewotmLocation] = useState('');
+    const [newotmCodepat, setNewotmCodepat] = useState('');
+
+    const [servicio, setServicio] = useState() // servicio hospitalario
+    const [problema, setProblema] = useState() // descripcion del problema
+
+    /* Get "user_id" from cookies*/
+    const cookies = parseCookies();
+    const users_id = cookies.users_id
+    const hospitals_id = cookies.hospitals_id
+
+    /* Datatable - useEffect */
     useEffect(() => {
         if (items.length > 0) {
             $('#example').DataTable();
@@ -27,15 +48,9 @@ const DataTableComponent = ({ items = [], setItems }) => {
     /* Router */
     const router = useRouter();
 
-    /* NEW OTM function */
-    const newOtm = (id) => {
-        router.push(`/newotm/${id}`);
-    };
-    /**********************/
-
     /* VIEW ITEM function */
     const viewItem = (id) => {
-        router.push(`/inventory/${id}`);
+        router.push(`/health/inventory/${id}`);
     };
     /**********************/
 
@@ -61,7 +76,6 @@ const DataTableComponent = ({ items = [], setItems }) => {
     }
     /* ********* */
 
-
     const updateItem = (e) => {
         e.preventDefault()
         fetch(`${config.apiUrl}/inventories/${editItemId}`, {
@@ -80,6 +94,85 @@ const DataTableComponent = ({ items = [], setItems }) => {
             })
             .catch(error => console.error('Error updating item:', error));
     };
+    /* ********* */
+
+    /* NEWOTM ITEM function */
+    const newOtm = (item) => {
+        //console.log(item)
+        openNewotmModal(item);
+    };
+
+    /* open and close modal for EDIT ITEM */
+    const openNewotmModal = (item) => {
+        setNewotmItemId(item._id);
+        setNewotmName(item.name);
+        setNewotmBrand(item.brand);
+        setNewotmModel(item.model);
+        setNewotmSerie(item.serie);
+        setNewotmLocation(item.location);
+        setNewotmCodepat(item.codepat);
+        setNewotmModalOpen(true);
+    }
+
+    const closeNewotmModal = () => {
+        setServicio('')
+        setProblema('')
+        setNewotmModalOpen(false);
+    }
+
+    const datePeru = new Date().toLocaleString('es-PE', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        timeZone: 'America/Lima'
+    });
+
+    const dateTimePeru = new Date().toLocaleString('es-PE', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'America/Lima'
+    });
+
+    const addotm = (e) => {
+        e.preventDefault()
+
+        fetch(`${config.apiUrl}/historials/create`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                hospitals_id: hospitals_id,
+                inventories_id: newotmItemId,
+                servicio: servicio,
+                problema: problema,
+                fecha_open: dateTimePeru,
+                estado: 'open',
+                usersid_open: users_id,
+            }),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    setResp(response)
+                    alert('Error al agregar OTM. Intente denuevo')
+                    throw Error('Error adding OTM')
+                }
+                return response.json()
+            })
+            .then(data => {
+                //console.log(data)
+                closeNewotmModal()
+                router.push('/health/otm')
+            })
+            .catch(error => {
+                console.error('Error adding OTM:', error)
+            });
+    }
+
+    /* ********* */
 
     const deleteItem = (id) => {
         if (window.confirm('Are you sure you want to delete this item?')) {
@@ -182,46 +275,151 @@ const DataTableComponent = ({ items = [], setItems }) => {
             )}
             {/******************/}
 
+            {/* NEW OTM modal */}
+            {newotmModalOpen && (
+                <div
+                    className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-70 z-50">
+                    <div className="p-8 bg-white rounded-lg shadow-lg m-4 sm:m-8 lg:w-1/2">
+                        <button onClick={closeNewotmModal} className="float-right text-gray-500 hover:text-gray-700">✖️
+                        </button>
+                        <form onSubmit={addotm}>
+                            <p>NEW OTM modal</p>
+                            <div className="mb-4">
+                                <label>Codigo Patrimonial</label>
+                                <input
+                                    type="text"
+                                    value={newotmCodepat}
+                                    onChange={(e) => setNewotmCodepat(e.target.value)}
+                                    className="px-3 py-2 border rounded bg-gray-300"
+                                    disabled
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label>Item Name</label>
+                                <input
+                                    type="text"
+                                    value={newotmName}
+                                    onChange={(e) => setNewotmName(e.target.value)}
+                                    className="px-3 py-2 border rounded bg-gray-300"
+                                    disabled
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label>Item Brand</label>
+                                <input
+                                    type="text"
+                                    value={newotmBrand}
+                                    onChange={(e) => setNewotmBrand(e.target.value)}
+                                    className="px-3 py-2 border rounded bg-gray-300"
+                                    disabled
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label>Item Model</label>
+                                <input
+                                    type="text"
+                                    value={newotmModel}
+                                    onChange={(e) => setNewotmModel(e.target.value)}
+                                    className="px-3 py-2 border rounded bg-gray-300"
+                                    disabled
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label>Item Serie</label>
+                                <input
+                                    type="text"
+                                    value={newotmSerie}
+                                    onChange={(e) => setNewotmSerie(e.target.value)}
+                                    className="px-3 py-2 border rounded bg-gray-300"
+                                    disabled
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label>Item Location</label>
+                                <input
+                                    type="text"
+                                    value={newotmLocation}
+                                    onChange={(e) => setNewotmLocation(e.target.value)}
+                                    className="px-3 py-2 border rounded bg-gray-300"
+                                    disabled
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label>Fecha de Solicitud</label>
+                                <input
+                                    type="text"
+                                    value={datePeru}
+                                    className="px-3 py-2 border rounded bg-gray-300"
+                                    disabled
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label htmlFor="">Servicio Hospitalario</label>
+                                <input type="text" className="px-3 py-2 border rounded"
+                                       onChange={(e) => setServicio(e.target.value)}
+                                       value={servicio}
+                                       required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label htmlFor="">Descripcion del Problema</label>
+                                <input type="text" className="px-3 py-2 border rounded"
+                                       onChange={(e) => setProblema(e.target.value)}
+                                       value={problema}
+                                       required
+                                />
+                            </div>
+
+                            <button className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Newotm
+                                Item
+                            </button>
+
+                        </form>
+                    </div>
+                </div>
+            )}
+            {/******************/}
+
             <div className="overflow-x-auto">
                 <table id="example" className="display">
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Brand</th>
-                            <th>Model</th>
-                            <th>Serie</th>
-                            <th>Location</th>
-                            <th>Codepat</th>
-                            <th>Action</th>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Brand</th>
+                        <th>Model</th>
+                        <th>Serie</th>
+                        <th>Location</th>
+                        <th>Codepat</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {items.map(item => (
+                        <tr key={item._id}>
+                            <td className="px-6 py-4 whitespace-nowrap">{item.name}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{item.brand}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{item.model}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{item.serie}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{item.location}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{item.codepat}</td>
+                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                <button onClick={() => newOtm(item)}
+                                        className="text-indigo-600 hover:text-indigo-900 m-1">NewOtm
+                                </button>
+                                <button onClick={() => viewItem(item._id)}
+                                        className="text-indigo-600 hover:text-indigo-900 m-1">View
+                                </button>
+                                <button onClick={() => editItem(item)}
+                                        className="text-indigo-600 hover:text-indigo-900 m-1">Edit
+                                </button>
+                                <button onClick={() => deleteItem(item._id)}
+                                        className="text-red-600 hover:text-red-900 m-1">Delete
+                                </button>
+                            </td>
                         </tr>
-                        </thead>
-                        <tbody>
-                        {items.map(item => (
-                            <tr key={item._id}>
-                                <td className="px-6 py-4 whitespace-nowrap">{item.name}</td>
-                                <td className="px-6 py-4 whitespace-nowrap">{item.brand}</td>
-                                <td className="px-6 py-4 whitespace-nowrap">{item.model}</td>
-                                <td className="px-6 py-4 whitespace-nowrap">{item.serie}</td>
-                                <td className="px-6 py-4 whitespace-nowrap">{item.location}</td>
-                                <td className="px-6 py-4 whitespace-nowrap">{item.codepat}</td>
-                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                                    <button onClick={() => newOtm(item._id)}
-                                            className="text-indigo-600 hover:text-indigo-900 m-1">NewOtm
-                                    </button>
-                                    <button onClick={() => viewItem(item._id)}
-                                            className="text-indigo-600 hover:text-indigo-900 m-1">View
-                                    </button>
-                                    <button onClick={() => editItem(item)}
-                                            className="text-indigo-600 hover:text-indigo-900 m-1">Edit
-                                    </button>
-                                    <button onClick={() => deleteItem(item._id)}
-                                            className="text-red-600 hover:text-red-900 m-1">Delete
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
+                    ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     );

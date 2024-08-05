32c19a98d00461be1003f63feca167d6220e6bf4
storres20 <sanchotorres777@gmail.com>
2024-08-04 23:52:09 -0500

    Update DatatableInventoryId.js

--- components/DatatableInventoryId.js
+++ components/DatatableInventoryId.js
@@ -2,18 +2,17 @@ import $ from 'jquery';
 import 'datatables.net';
 import 'datatables.net-dt/css/dataTables.dataTables.css';
 
-import config from '@/config'; //for apiUrl
+import config from '@/config'; // for apiUrl
 
-import {useEffect, useState} from 'react';
-import {parseCookies} from "nookies";
+import { useEffect, useState } from 'react';
+import { parseCookies } from "nookies";
 
-const DataTableComponent = ({ id = [] }) => {
-
+const DataTableComponent = ({ id }) => {
     const [item, setItem] = useState(null);
     const [historyData, setHistoryData] = useState(null);
 
     /* useState - newotm item */
-    const [newotmModalOpen, setNewotmModalOpen] = useState(false)
+    const [newotmModalOpen, setNewotmModalOpen] = useState(false);
     const [newotmItemId, setNewotmItemId] = useState(null);
     const [newotmName, setNewotmName] = useState('');
     const [newotmBrand, setNewotmBrand] = useState('');
@@ -22,16 +21,23 @@ const DataTableComponent = ({ id = [] }) => {
     const [newotmLocation, setNewotmLocation] = useState('');
     const [newotmCodepat, setNewotmCodepat] = useState('');
 
-    const [servicio, setServicio] = useState() // servicio hospitalario
-    const [problema, setProblema] = useState() // descripcion del problema
-    /*******************/
+    const [servicio, setServicio] = useState(''); // servicio hospitalario
+    const [problema, setProblema] = useState(''); // descripcion del problema
 
-    /* Get "user_id" from cookies*/
+    /* Get "user_id" from cookies */
     const cookies = parseCookies();
-    const users_id = cookies.users_id
-    const hospitals_id = cookies.hospitals_id
-    /*******************/
+    const users_id = cookies.users_id;
+    const hospitals_id = cookies.hospitals_id;
 
+    const fetchHistoryData = async () => {
+        try {
+            const response = await fetch(`${config.apiUrl}/historials/getByInventory/${id}`);
+            const data = await response.json();
+            setHistoryData(data);
+        } catch (error) {
+            console.error('Error fetching history:', error);
+        }
+    };
 
     useEffect(() => {
         if (historyData && historyData.length > 0) {
@@ -46,17 +52,12 @@ const DataTableComponent = ({ id = [] }) => {
                 .then(data => setItem(data))
                 .catch(error => console.error('Error fetching item:', error));
 
-            fetch(`${config.apiUrl}/historials/getByInventory/${id}`)
-                .then(response => response.json())
-                .then(data => setHistoryData(data))
-                .catch(error => console.error('Error fetching history:', error));
+            fetchHistoryData();
         }
     }, [id]);
 
-
     /* NEWOTM ITEM function */
     const newOtm = (item) => {
-        //console.log(item)
         openNewotmModal(item);
     };
 
@@ -70,13 +71,13 @@ const DataTableComponent = ({ id = [] }) => {
         setNewotmLocation(item.location);
         setNewotmCodepat(item.codepat);
         setNewotmModalOpen(true);
-    }
+    };
 
     const closeNewotmModal = () => {
-        setServicio('')
-        setProblema('')
+        setServicio('');
+        setProblema('');
         setNewotmModalOpen(false);
-    }
+    };
 
     const datePeru = new Date().toLocaleString('es-PE', {
         day: '2-digit',
@@ -95,7 +96,7 @@ const DataTableComponent = ({ id = [] }) => {
     });
 
     const addotm = (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
         fetch(`${config.apiUrl}/historials/create`, {
             method: 'POST',
@@ -114,41 +115,30 @@ const DataTableComponent = ({ id = [] }) => {
         })
             .then(response => {
                 if (!response.ok) {
-                    setResp(response)
-                    alert('Error al agregar OTM. Intente denuevo')
-                    throw Error('Error adding OTM')
+                    alert('Error al agregar OTM. Intente denuevo');
+                    throw Error('Error adding OTM');
                 }
-                return response.json()
+                return response.json();
             })
-            .then(data => {
-                //console.log(data)
-                setItem(data)
-                closeNewotmModal()
-                //router.push('/health/otm')
+            .then(() => {
+                closeNewotmModal();
+                fetchHistoryData(); // Fetch updated history data
             })
             .catch(error => {
-                console.error('Error adding OTM:', error)
+                console.error('Error adding OTM:', error);
             });
-    }
-
-    /* ********* */
+    };
 
     // Check if the details and historyData have been set. If not, still loading.
     if (!item || !historyData) return <div>Loading...</div>;
 
     return (
-    // The table structure goes here
-    // Example:
         <div>
-
             {/* NEW OTM modal */}
             {newotmModalOpen && (
-                <div
-                    className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-70 z-50 overflow-y-auto">
-                    <div
-                        className="p-8 bg-white rounded-lg shadow-lg m-4 sm:m-8 lg:w-1/2 max-h-screen overflow-y-auto mt-4 md:mt-12">
-                        <button onClick={closeNewotmModal} className="float-right text-gray-500 hover:text-gray-700">✖️
-                        </button>
+                <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-70 z-50 overflow-y-auto">
+                    <div className="p-8 bg-white rounded-lg shadow-lg m-4 sm:m-8 lg:w-1/2 max-h-screen overflow-y-auto mt-4 md:mt-12">
+                        <button onClick={closeNewotmModal} className="float-right text-gray-500 hover:text-gray-700">✖️</button>
                         <form onSubmit={addotm}>
                             <p>NEW OTM modal</p>
                             <div className="mb-4">
@@ -237,10 +227,7 @@ const DataTableComponent = ({ id = [] }) => {
                                 />
                             </div>
 
-                            <button className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Newotm
-                                Item
-                            </button>
-
+                            <button className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Newotm Item</button>
                         </form>
                     </div>
                 </div>
@@ -255,7 +242,7 @@ const DataTableComponent = ({ id = [] }) => {
             <p>Serie: {item.serie}</p>
             <p>Location: {item.location}</p>
             <p>Codepat: {item.codepat}</p>
-            <br/>
+            <br />
             <h2>History</h2>
             {historyData.length > 0 ? (
                 <div className="overflow-x-auto">

6c5acea5b56eaedb25064105f1f9c20b05aaa553
storres20 <sanchotorres777@gmail.com>
2024-08-15 22:31:48 -0500

    Refactor page

--- app/health/page.js
+++ app/health/page.js
@@ -4,6 +4,7 @@
 import dynamic from 'next/dynamic';
 import { parseCookies } from 'nookies';
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import QrCodeReader from '@/components/QrCodeReader';
 
 // Dynamic Import
@@ -15,6 +16,7 @@ const DataTableComponent = dynamic(
 export default function HealthPage() {
     const cookies = parseCookies();
     const profile = cookies.profile;
+    const router = useRouter(); // Add the useRouter hook
 
     const [result, setResult] = useState('');
     const [hydrated, setHydrated] = useState(false);
@@ -22,6 +24,13 @@ export default function HealthPage() {
     useEffect(() => {
         setHydrated(true);
     }, []);
+
+    useEffect(() => {
+        if (result) {
+            // Redirect to the new page
+            router.push(`/health/${result}`);
+        }
+    }, [result, router]); // Trigger when result changes
 
     if (!hydrated) {
         return null; // or a loading indicator
@@ -31,20 +40,11 @@ export default function HealthPage() {
         return <p>You do not have access to this page.</p>;
     }
 
-
     return (
         <div className="p-8">
             <h1>Health Content</h1>
             <h2>QR Code Reader</h2>
             <QrCodeReader setResult={setResult} />
-            <div>
-                <h2 className="text-xl font-semibold">Scanned Result:</h2>
-                <p className="mt-2 text-lg">{result}</p>
-            </div>
-            {/* DataTable*/}
-            {result && (
-                <DataTableComponent id={result}/>
-            )}
         </div>
     );
 }

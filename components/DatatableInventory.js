5c3f3127179be24ebefe70e684b7e0417738c96b
storres20 <sanchotorres777@gmail.com>
2024-08-15 22:28:44 -0500

    Add new cookie for location

--- components/DatatableInventory.js
+++ components/DatatableInventory.js
@@ -37,6 +37,7 @@ const DataTableComponent = ({ items = [], setItems }) => {
     const cookies = parseCookies();
     const users_id = cookies.users_id
     const hospitals_id = cookies.hospitals_id
+    const location = cookies.location
 
     /* Datatable - useEffect */
     useEffect(() => {
@@ -391,35 +392,39 @@ const DataTableComponent = ({ items = [], setItems }) => {
                         <th>Model</th>
                         <th>Serie</th>
                         <th>Location</th>
+                        <th>Sub Location</th>
                         <th>Codepat</th>
                         <th>Action</th>
                     </tr>
                     </thead>
                     <tbody>
-                    {items.map(item => (
-                        <tr key={item._id}>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.name}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.brand}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.model}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.serie}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.location}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{item.codepat}</td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                                {/*<button onClick={() => newOtm(item)}
+                    {items
+                        .filter(item => item.location === location)  // Filter by location
+                        .map(item => (
+                            <tr key={item._id}>
+                                <td className="px-6 py-4 whitespace-nowrap">{item.name}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{item.brand}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{item.model}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{item.serie}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{item.location}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{item.sub_location}</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{item.codepat}</td>
+                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                    {/*<button onClick={() => newOtm(item)}
                                         className="text-indigo-600 hover:text-indigo-900 m-1">NewOtm
                                 </button>*/}
-                                <button onClick={() => viewItem(item._id)}
-                                        className="text-indigo-600 hover:text-indigo-900 m-1">View
-                                </button>
-                                {/*<button onClick={() => editItem(item)}
+                                    <button onClick={() => viewItem(item._id)}
+                                            className="text-indigo-600 hover:text-indigo-900 m-1">View
+                                    </button>
+                                    {/*<button onClick={() => editItem(item)}
                                         className="text-indigo-600 hover:text-indigo-900 m-1">Edit
                                 </button>
                                 <button onClick={() => deleteItem(item._id)}
                                         className="text-red-600 hover:text-red-900 m-1">Delete
                                 </button>*/}
-                            </td>
-                        </tr>
-                    ))}
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
